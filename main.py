3442260d48fd6035f810fafabc60f8088b3537bf
roberte777 <robewilkes@yahoo.com>
2024-02-03 01:03:48 -0600

    feature: account details

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.requests import StockQuotesRequest, StockBarsRequest
+from alpaca.data.requests import (
+    StockLatestQuoteRequest,
+    StockBarsRequest,
+)
 from alpaca.data.timeframe import TimeFrame
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
 from datetime import datetime, timedelta
 from pytz import timezone
 from dotenv import load_dotenv
@@ -18,6 +24,7 @@ client = StockHistoricalDataClient(
     api_key=os.getenv("API_KEY"), secret_key=os.getenv("API_SECRET")
 )
 
+
 # get todays date
 def subtract_trading_days(start_date, trading_days):
     # Assuming trading days are Monday to Friday
@@ -28,34 +35,38 @@ def subtract_trading_days(start_date, trading_days):
             trading_days -= 1
     return current_date
 
+
 today = datetime.now(timezone("America/New_York"))
 t_minus22 = subtract_trading_days(today, 25)
 
 print(today, t_minus22)
 
 request = StockBarsRequest(
-    symbol_or_symbols=[TKR], start=t_minus22, end=today, timeframe=TimeFrame.Day, feed = 'iex'
+    symbol_or_symbols=[TKR],
+    start=t_minus22,
+    end=today,
+    timeframe=TimeFrame.Day,
+    feed="iex",
 )
 
 print("Requesting data...")
 
 
-
 result = client.get_stock_bars(request)
 # print(result.df)
 
-vwap = pd.DataFrame(result.df['vwap'])
-vwap['diff'] = result.df['vwap'].diff(1).shift(-1)
-vwap['rm'] = vwap['diff']/vwap['vwap']
+vwap = pd.DataFrame(result.df["vwap"])
+vwap["diff"] = result.df["vwap"].diff(1).shift(-1)
+vwap["rm"] = vwap["diff"] / vwap["vwap"]
 
 print(vwap)
 
 # get standard deviation of the vwap
 
-std = vwap['rm'].std()
+std = vwap["rm"].std()
 print(std)
 
-annual_vol = std * (252 ** 0.5)
+annual_vol = std * (252**0.5)
 
 leverage = TARGET_VOL / annual_vol
 
@@ -64,10 +75,25 @@ print(f"Target Volatility: {TARGET_VOL:.2f}")
 print(f"Leverage: {leverage:.2f}")
 
 
-
-
-
 # At this point we want to hold SPY with a leverage of our variable leverage
 # We can put anything not in SPY into treasuries
 
 
+trading_client = TradingClient(os.getenv("API_KEY"), os.getenv("API_SECRET"))
+latest_quote_request = StockLatestQuoteRequest(symbol_or_symbols=TKR)
+latest_quote = client.get_stock_latest_quote(latest_quote_request)
+positions = trading_client.get_all_positions()
+buying_power = trading_client.get_account().buying_power
+cash = trading_client.get_account().cash
+print(f"Buying Power: {buying_power}")
+print(f"Positions: {positions}")
+print(f"Cash: {cash}")
+
+
+# preparing order data
+market_order_data = MarketOrderRequest(
+    symbol=TKR, qty=0.0001, side=OrderSide.BUY, time_in_force=TimeInForce.DAY
+)
+
+# Market order
+# market_order = trading_client.submit_order(order_data=market_order_data)
